841571b610a0cc48c1695a5d999e13509a3aa8cc
Joe Polo <joe.polo@telus.com>
2022-11-23 16:04:15 -0800

    added cross matrix wordsearch to search for strings in vertical and horizontal 2d arrays

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,27 @@
 const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
-
-module.exports = wordSearch
+    
+    let verticalJoin = [];
+  
+      for (let i = 0; i < letters[0].length; i++) {
+        const col =[]
+        for(let j = 0; j < letters.length; j++) {
+            col.push(letters[j][i]);
+          }
+          verticalJoin.push(col);
+      }
+      verticalJoin = verticalJoin.map(ls => ls.join(''));
+  
+      const horizontalJoin = letters.map(ls => ls.join(''))
+  
+      const thirdArray = verticalJoin.concat(horizontalJoin);
+  
+      for (l of thirdArray) {
+          if (l.includes(word)) return true;
+      }
+      for (j of thirdArray)   {
+          
+          if (!l.includes(word)) return false;
+      }
+  }
+  
+  module.exports = wordSearch
